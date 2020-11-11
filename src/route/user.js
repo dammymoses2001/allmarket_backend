76f278aee2f25cd61d82a0abfdb7c81148fb4429
Dammy <dammymoses2001@gmail.com>
2020-11-11 09:14:27 +0100

    update

--- src/route/user.js
+++ src/route/user.js
@@ -2,15 +2,31 @@ const express = require('express');
 const knex = require('knex');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+
+
 const db = knex({
+
   client: 'pg',
   connection: {
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
+    host: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+    //host: process.env.HOST,
+    // user: process.env.USER,
+    // password: process.env.PASSWORD,
+    // database: process.env.DATABASE,
   },
 });
+// const db = knex({
+//   client: 'pg',
+//   connection: {
+//     host: process.env.HOST,
+//     user: process.env.USER,
+//     password: process.env.PASSWORD,
+//     database: process.env.DATABASE,
+//   },
+// });
 const Login = require('../controllers/Login');
 const Register = require('../controllers/Register');
 const route = express.Router();
